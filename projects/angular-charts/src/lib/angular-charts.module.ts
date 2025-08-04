bfb4947c76e74ba0fdc14979709b192e6a26c1e5
Z8PMD <pmd@kmd.dk>
2025-08-04 15:57:30 +0530

    scatter chart added

--- projects/angular-charts/src/lib/angular-charts.module.ts
+++ projects/angular-charts/src/lib/angular-charts.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { ScatterChartComponent } from './components/scatter-chart/scatter-chart.component';
 
 @NgModule({
   imports: [
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ScatterChartComponent
   ],
   exports: [
     LineChartComponent,
-    PieChartComponent
+    PieChartComponent,
+    ScatterChartComponent
   ],
+  declarations: []
 })
 export class AngularChartsModule { }
---
+++ projects/angular-charts/src/lib/components/scatter-chart/scatter-chart.stories.ts
@@ -0,0 +1,56 @@
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { ScatterChartComponent } from './scatter-chart.component';
+import { NgxEchartsModule } from 'ngx-echarts';
+
+const meta: Meta<ScatterChartComponent> = {
+  title: 'Charts/ScatterChart',
+  component: ScatterChartComponent,
+  decorators: [
+    moduleMetadata({
+      imports: [
+        NgxEchartsModule.forRoot({
+          echarts: () => import('echarts')
+        })
+      ]
+    })
+  ],
+  tags: ['autodocs'],
+};
+
+export default meta;
+
+type Story = StoryObj<ScatterChartComponent>;
+
+export const ScatterChart: Story = {
+  args: {
+    chartOptions: {
+      title: {
+        text: 'Sample Scatter Chart'
+      },
+      tooltip: { trigger: 'item' },
+      xAxis: { type: 'value' },
+      yAxis: { type: 'value' },
+      series: [
+        {
+          name: 'Data Points',
+          type: 'scatter',
+          data: [
+            [10, 8],
+            [15, 12],
+            [15, 14],
+            [13, 12],
+            [16, 10],
+            [15, 15],
+            [9, 12],
+            [10, 10],
+            [20, 18],
+            [25, 24],
+            [30, 28],
+            [35, 36],
+            [40, 42]
+          ]
+        }
+      ]
+    }
+  },
+};
