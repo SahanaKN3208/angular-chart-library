fed2bddb85aeb4ac102ce26846f861ead5c0ce90
Z8GGO <ggo@kmd.dk>
2025-08-06 12:58:38 +0530

    Package bundling using npm pack

--- projects/angular-charts/src/lib/angular-charts.module.ts
+++ projects/angular-charts/src/lib/angular-charts.module.ts
@@ -2,17 +2,20 @@ import { NgModule } from '@angular/core';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { ScatterChartComponent } from './components/scatter-chart/scatter-chart.component';
+import {BarChartComponent} from "./components/bar-chart/bar-chart.component";
 
 @NgModule({
   imports: [
     LineChartComponent,
     PieChartComponent,
-    ScatterChartComponent
+    ScatterChartComponent,
+    BarChartComponent
   ],
   exports: [
     LineChartComponent,
     PieChartComponent,
-    ScatterChartComponent
+    ScatterChartComponent,
+    BarChartComponent
   ]
 })
 export class AngularChartsModule { }
