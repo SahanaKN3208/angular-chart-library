7a8e60ddbe4b121cec7bacee851432d48790e193
Z8PKJ <pkj@kmd.dk>
2025-08-07 15:45:38 +0530

    unwanted changes removed.

--- projects/angular-charts/src/lib/angular-charts.module.ts
+++ projects/angular-charts/src/lib/angular-charts.module.ts
@@ -3,12 +3,7 @@ import { LineChartComponent } from './components/line-chart/line-chart.component
 import { PieChartComponent } from './components/pie-chart/pie-chart.component';
 import { ScatterChartComponent } from './components/scatter-chart/scatter-chart.component';
 import {BarChartComponent} from "./components/bar-chart/bar-chart.component";
-import * as echarts from 'echarts/core';
-import { BarChart ,LineChart,PieChart,ScatterChart} from 'echarts/charts';
-import { GridComponent } from 'echarts/components';
-import { CanvasRenderer } from 'echarts/renderers';
 import {AreaChartComponent} from "./components/area-chart/area-chart.component";
-echarts.use([BarChart,LineChart,PieChart,ScatterChart ,GridComponent, CanvasRenderer])
 
 @NgModule({
   imports: [
