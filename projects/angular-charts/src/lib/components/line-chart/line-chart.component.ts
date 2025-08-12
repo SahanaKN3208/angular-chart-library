f09fb9a401fcd2ab08768abd3c85b1f3f3537d2c
Z8PMD <pmd@kmd.dk>
2025-08-12 12:06:31 +0530

    Strict type for line chart

--- projects/angular-charts/src/lib/components/line-chart/line-chart.component.ts
+++ projects/angular-charts/src/lib/components/line-chart/line-chart.component.ts
@@ -1,47 +1,45 @@
-import {Component, Input} from '@angular/core';
-import { EChartsOption } from 'echarts';
-import {NgxEchartsDirective,NgxEchartsModule} from "ngx-echarts";
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { EChartsOption, SeriesOption } from 'echarts';
+import {NgxEchartsDirective, NgxEchartsModule } from 'ngx-echarts';
 
 @Component({
   selector: 'ddip-line-chart',
   standalone: true,
-  imports: [
-    NgxEchartsDirective,NgxEchartsModule
-  ],
+  imports: [NgxEchartsDirective, NgxEchartsModule],
   templateUrl: './line-chart.component.html',
   styleUrls: ['./line-chart.component.css']
 })
-export class LineChartComponent {
-  @Input() chartOptions: EChartsOption = {
-    title: {
-      text: 'Line Chart'
-    },
-    tooltip: {
-      trigger: 'axis'
-    },
-    legend:{
-      data:['Visits'],
-      show:true,
-      left: 'right'
-    },
-    xAxis: {
-      type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    },
-    yAxis: {
-      type: 'value'
-    },
-    series: [
-      {
-        name: 'Visits',
-        type: 'line',
-        data: [120, 200, 150, 80, 70, 110, 130],
-        smooth: true
-      }
+export class LineChartComponent implements OnChanges {
 
-    ]
-  };
-  @Input() initOption: any = {
-    renderer: 'canvas'
+  @Input() seriesData: { name: string; data: number[]; smooth?: boolean }[] = [];
+  @Input() categories: string[] = [];
+  @Input() title?: EChartsOption['title'];
+  @Input() tooltip?: EChartsOption['tooltip'];
+  @Input() legend?: EChartsOption['legend'];
+  @Input() xAxis?: EChartsOption['xAxis'];
+  @Input() yAxis?: EChartsOption['yAxis'];
+
+  chartOptions: EChartsOption = {};
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.updateChartOptions();
+  }
+
+  private updateChartOptions(): void {
+    const series: SeriesOption[] = this.seriesData.map(item => ({
+      type: 'line',
+      name: item.name,
+      data: item.data,
+      smooth: item.smooth ?? false
+    }));
+
+    this.chartOptions = {
+      title: this.title,
+      tooltip: this.tooltip ?? { trigger: 'axis' },
+      legend: this.legend,
+      xAxis: this.xAxis ?? { type: 'category', data: this.categories },
+      yAxis: this.yAxis ?? { type: 'value' },
+      series
+    };
   }
 }
--- projects/angular-charts/src/lib/components/line-chart/line-chart.stories.ts
+++ projects/angular-charts/src/lib/components/line-chart/line-chart.stories.ts
@@ -1,16 +1,19 @@
-import {Meta, moduleMetadata, StoryObj} from '@storybook/angular';
-import {LineChartComponent} from "./line-chart.component";
-import {NgxEchartsDirective, NgxEchartsModule} from "ngx-echarts";
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
+import { LineChartComponent } from './line-chart.component';
+import { NgxEchartsDirective, NgxEchartsModule } from 'ngx-echarts';
 
 const meta: Meta<LineChartComponent> = {
   title: 'Charts/LineChart',
   component: LineChartComponent,
   decorators: [
     moduleMetadata({
-      imports: [NgxEchartsModule.forRoot({
-        echarts: () => import('echarts')
-      }),NgxEchartsDirective]
-    })
+      imports: [
+        NgxEchartsModule.forRoot({
+          echarts: () => import('echarts'),
+        }),
+        NgxEchartsDirective,
+      ],
+    }),
   ],
   tags: ['autodocs'],
 };
@@ -21,36 +24,15 @@ type Story = StoryObj<LineChartComponent>;
 
 export const LineChart: Story = {
   args: {
-    chartOptions: {
-      title: {
-        text: 'Line Chart'
-      },
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend:{
-        data:['Visits'],
-        show: true,
-        left: 'right'
-      },
-      xAxis: {
-        type: 'category',
-        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          name: 'Visits',
-          type: 'line',
-          data: [120, 200, 150, 80, 70, 110, 130],
-          smooth: true
-        }
-      ]
-    },
-    initOption: {
-      renderer: 'canvas'
-    }
-  }
+    seriesData: [
+      { name: 'Visits', data: [120, 200, 150, 80, 70, 110, 130], smooth: true },
+      { name: 'Sales', data: [90, 140, 180, 100, 60, 80, 120] },
+    ],
+    categories: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    title: { text: 'Weekly Performance' },
+    tooltip: { trigger: 'axis' },
+    legend: { data: ['Visits', 'Sales'], show: true, left: 'right' },
+    xAxis: { type: 'category', data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'] },
+    yAxis: { type: 'value' },
+  },
 };
